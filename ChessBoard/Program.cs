8ffa991037639384b4c6b7ca2d58baf7cb87121e
Trucksson <Henric.kurtsson@gmail.com>
2024-11-17 23:46:12 +0100

    Add project files.

---
+++ ChessBoard/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Skriv in ett tal");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;//Hanterar specialtecken
+
+            int number;
+
+            while (!int.TryParse(Console.ReadLine(), out number) || number <= 0) //En while loop för att ha ett positivt heltal 
+            {               
+            }
+            for (int row = 0; row < number; row++) //Loop som skriver ut rader
+            {
+                for (int col = 0; col < number; col++) //Loop som skriver ut Kollumner
+                {
+                    if ((row + col) % 2 == 0) //If sats
+                    {
+                        Console.Write("◻︎");
+                    }
+                    else
+                    {
+                        Console.Write("◼︎");
+                    }
+                }
+                Console.WriteLine();  
+            }
+
+            Console.ReadLine();  //Håller konsolen öppen
+        }
+    }
+}
